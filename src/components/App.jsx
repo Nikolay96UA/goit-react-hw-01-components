3da2cbc85c4b29f1ec167798b6237350584383aa
Nikolay Maloshenko <115625550+Nikolay96UA@users.noreply.github.com>
2023-05-20 00:04:29 +0300

    Task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
+import { Profile } from './SocialProfile/SocialProfile';
+import user from './SocialProfile/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Profile
+      key={user.username}
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
   );
 };
